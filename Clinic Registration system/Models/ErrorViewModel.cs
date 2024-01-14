e9ff48284b3256bfbaf558ce4a4c15da25d6b1d8
sheikha ALQuyudhi <alqyoodhi181@gmail.com>
2024-01-14 12:31:25 -0800

    the repository and files created by sheikha

---
+++ Clinic Registration system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clinic_Registration_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
